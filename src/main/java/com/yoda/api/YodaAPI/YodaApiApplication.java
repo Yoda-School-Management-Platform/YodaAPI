92f53183e25011e6fe9d8d22c60cd93887c14154
iLePix <31792282+iLePix@users.noreply.github.com>
2020-01-11 18:11:42 +0100

    Initial commit

---
+++ src/main/java/com/yoda/api/YodaAPI/YodaApiApplication.java
@@ -0,0 +1,13 @@
+package com.yoda.api.YodaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YodaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YodaApiApplication.class, args);
+	}
+
+}
