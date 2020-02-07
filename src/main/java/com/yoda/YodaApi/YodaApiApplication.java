89f03ceaf89e5a4fd6baaf96e073e94d955f4cf8
Malex14 <malte.alexander.behrmann@web.de>
2020-02-07 22:27:03 +0100

    07.02.2020 22:26

---
+++ src/main/java/com/yoda/YodaApi/YodaApiApplication.java
@@ -0,0 +1,13 @@
+package com.yoda.YodaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YodaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YodaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoda/YodaApi/YodaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoda.YodaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YodaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
